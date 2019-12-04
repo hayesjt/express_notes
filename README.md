b702277e40ffd40151e418a6f628f0256de70410
haylee tk <haylee430@gmail.com>
2019-12-03 20:09:13 -0600

    app complete

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # express_notes
 An App that allows you to take notes, save notes, and delete notes
+
+ # OBJECTIVE
+
+The front end files were given and the purpose of this assignment was supposed to create the back end files.
+
+The backend files allowed for the server to be used, to post notes, get notes, and delete notes. 
+
+# files and discription 
+
+db.json - files to hold all note (pushes go to this file)
+public folder - all front end files (given)
+routes -
+    apiroutes - all the functions that are running the push and delete funcitons with db.json
+    htmlroutes - all routes so url directs to correct places
+app.js - runs entire app and sets up server
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const fs = require("fs");
 
 // GETTING ALL NOTES IN DB.JSON FILE TO DISPLAY ON SCREEN
-router.get("/notes", function(req, res){
-    fs.readFile("db/db.json", "utf8", function(err, data){
+router.get("/notes", function (req, res) {
+    fs.readFile("db/db.json", "utf8", function (err, data) {
         if (err) {
             console.log("File read failed: " + err);
             return
@@ -13,66 +13,69 @@ router.get("/notes", function(req, res){
 });
 
 // POSTING NEW NOTES TO POST TO DB.JSON IN ORDER TO SHOW ON PAGE
-router.post("/notes", function (req, res){
-    
-    fs.readFile("db/db.json", "utf8", function(err, data){
+router.post("/notes", function (req, res) {
+
+    // READING THE FILE FIRST SO WE DONT OVERRIDE OTHER DATA
+    fs.readFile("db/db.json", "utf8", function (err, data) {
         if (err) {
             console.log("File read failed: " + err);
             return
         }
         let origData = JSON.parse(data);
         lastID = 0;
-        for (i = 0; i<origData.length; i++){
+        for (i = 0; i < origData.length; i++) {
             lastID = origData[i].id;
         }
-        
+
+        // GIVING AN ID TO THE NOTE SO IT CAN DISPLAY
         let newNote = req.body;
 
         lastID++;
         newNote.id = lastID;
 
-         origData.push(newNote);
+        origData.push(newNote);
 
-        //write fs 
-  
-        fs.writeFile('db/db.json', JSON.stringify(origData), 'utf8',function(err){
+       
+        // WRITING TO JSON FILE SO IT CAN DISPLAY ON WEBSITE
+        fs.writeFile('db/db.json', JSON.stringify(origData), 'utf8', function (err) {
             if (err) throw err;
             console.log('The file has been saved!');
             res.json(origData)
         });
 
     });
-   
-   
+
+
 });
 
 // DELETING OLD NOTES FROM DB.JSON SO THEY ARE NOT SHOWN ON THE SCREEN
-router.delete("/notes/:id", function(req, res){
+router.delete("/notes/:id", function (req, res) {
 
-    fs.readFile("db/db.json", "utf8", function(err, data){
+    // READING THE FILE FIRST SO WE DONT OVERRIDE OTHER DATA
+    fs.readFile("db/db.json", "utf8", function (err, data) {
         if (err) {
             console.log("File read failed: " + err);
             return
         }
         let origData = JSON.parse(data);
         lastID = 0;
-        for (i = 0; i<origData.length; i++){
+        for (i = 0; i < origData.length; i++) {
             console.log(origData[i].id, req.params.id);
-            if (origData[i].id === parseInt(req.params.id)){
-               origData.splice(i, 1)
-               console.log("slice hit")
-            }          
+            if (origData[i].id === parseInt(req.params.id)) {
+                origData.splice(i, 1)
+                console.log("slice hit")
+            }
         }
 
-        //write fs 
-        fs.writeFile('db/db.json', JSON.stringify(origData), 'utf8',function(err){
+        //WRITING FILE SO IT WILL BE TAKEN OFF WEBISTE  
+        fs.writeFile('db/db.json', JSON.stringify(origData), 'utf8', function (err) {
             if (err) throw err;
             console.log('The file has been deleted!');
             res.json(origData)
         });
 
     });
-   
+
 });
 
 module.exports = router 
