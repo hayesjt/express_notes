ce6f2cdce823a9b19c843c214a72ce792c3680c2
Haylee Thomas-Kuhlmann <hay.design.lee@gmail.com>
2020-03-17 21:34:59 -0500

    explained routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const router = require("express").Router();
 
-
+// route to note HTML page - only with /notes
 router.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
 
+// anything else goes to main page
 router.get("*", function(req, res){
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
